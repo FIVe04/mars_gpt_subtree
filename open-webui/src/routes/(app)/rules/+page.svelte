4cb8abe4a3fea5d77b161ea165b104bdbbe2bd12
FIVe04 <ivan.a.frolov@gmail.com>
2025-09-17 03:12:59 +0300

    added rules page

---
+++ open-webui/src/routes/(app)/rules/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    // You can add script logic here if needed
+</script>
+
+<style>
+    h1 {
+        color: #2d72d9;
+        font-family: sans-serif;
+    }
+</style>
+
+<h1>Hello, World!</h1>
